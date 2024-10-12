cc4d6c9c9d272b52e41995f27f4a38be5d0f3372
Eduardo-Brando-Almeida <dugu2005@gmail.com>
2024-10-11 22:09:36 -0300

    mudança do texto da connection

--- app/models/db.js
+++ app/models/db.js
@@ -5,15 +5,15 @@ var connection = mysql.createConnection({
   host: dbConfig.HOST,
   user: dbConfig.USER,
   password: dbConfig.PASSWORD,
-  database: dbConfig.DB
+  database: dbConfig.DB,
 });
 
 connection.connect((err) => {
   if (err) {
-    console.error('Erro ao conectar: ' + err.stack);
+    console.error("Erro ao tentar conectar: " + err.stack);
     return;
   }
-  console.log('Conectado como ID ' + connection.threadId);
+  console.log("Conectado como ID " + connection.threadId);
 });
 
 module.exports = connection;
